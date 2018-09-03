f1ab96b77e8e6b647b886dc4861a378c6ff1441a
stopiccot <alexey.petruchik@gmail.com>
2018-09-03 12:30:23 +0300

    ololo

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -4,16 +4,15 @@ project="<YOUR PROJECT NAME HERE>"
 
 echo "Attempting to build $project for OS X"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity 
-  -batchmode 
-  -nographics 
-  -silent-crashes 
-  -logFile $(pwd)/unity.log 
-  -projectPath $(pwd) 
-  -buildOSXUniversalPlayer "$(pwd)/Build/osx/$test.app" 
   -quit
+  -batchmode
+  -executeMethod AutoBuilder.PerformiOSBuildSimulator
 
 echo 'Logs from build'
-cat $(pwd)/unity.log
+cat $(pwd)/unity.
+
+cat $(pwd)/Builds/iOS 
+ls $(pwd)/Builds/iOS
 
 # echo 'Attempting to zip builds'
 # zip -r $(pwd)/Build/linux.zip $(pwd)/Build/linux/
